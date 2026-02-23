874920e467fec36aad49b5ae9c62aeb2dbc65c42
Joana <yopiixf@gmail.com>
2026-02-23 17:42:25 +0000

    Creación FireBase js

---
+++ src/services/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCE4a2bo_KNY_opyFBy4thBpR8eckUV04",
+  authDomain: "proyect-deadbydayligth.firebaseapp.com",
+  projectId: "proyect-deadbydayligth",
+  storageBucket: "proyect-deadbydayligth.firebasestorage.app",
+  messagingSenderId: "835268103515",
+  appId: "1:835268103515:web:8b9d4e70360ac00afc5e7e"
+};
+
+const app = initializeApp(firebaseConfig);
